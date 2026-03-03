76fd2604f391cf06e48c71a421a31b2a621b5cae
Ann David <annapauladav@gmail.com>
2026-03-03 08:31:02 +0100

    Commit new changes

--- server.js
+++ server.js
@@ -148,6 +148,8 @@ app.post("/verify-payment", async (req, res) => {
 
 /* ================= START SERVER ================= */
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
