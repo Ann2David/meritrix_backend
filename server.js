8e0710a609a1adec479eb539533ab1820aca1605
Ann David <annapauladav@gmail.com>
2026-03-24 16:48:49 +0100

    Add new

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
 
 /* ================= GOOGLE CALENDAR CONFIG ================= */
 const auth = new google.auth.GoogleAuth({
@@ -211,6 +210,10 @@ app.get("/health", (req, res) => {
 });
 
 
+// Use the port Render gives you, or default to 3000
+const PORT = process.env.PORT || 3000;
+
+// CRITICAL: You must include '0.0.0.0' so Render can "see" the port
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server is officially live on port ${PORT}`);
 });
