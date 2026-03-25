285ff505fc6d4f07937c12c6d37cb52dfa7cdfc2
Ann David <annapauladav@gmail.com>
2026-03-25 08:15:08 +0100

    recent change

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ async function createCalendarEvent(name, email, appointmentString, duration) {
             conferenceData: {
                 createRequest: { 
                     requestId: `mtx-${Date.now()}`, 
-                    conferenceSolutionKey: { type: 'hangoutsMeet' } 
+                    conferenceSolutionKey: { type: 'meet' } 
                 }
             },
         };
